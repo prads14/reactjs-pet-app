9009dd1268a97eae28e24838f0a9cd23eea4b5ad
prads14 <pradnyaprachi@gmail.com>
2024-01-16 14:17:44 +0530

    fixed http https protocol

--- src/fetchPet.js
+++ src/fetchPet.js
@@ -1,6 +1,6 @@
 const fetchPet = async ({ queryKey }) => {
   const id = queryKey[1];
-  const apiRes = await fetch(`http://pets-v2.dev-apis.com/pets?id=${id}`);
+  const apiRes = await fetch(`https://pets-v2.dev-apis.com/pets?id=${id}`);
 
   if (!apiRes.ok) {
     throw new Error(`details/${id} response is not correct.`);
@@ -14,7 +14,7 @@ export default fetchPet;
 //   let id = 1;
 //   let obj;
 
-//   const res = await fetch(`http://pets-v2.dev-apis.com/pets?id=${id}`);
+//   const res = await fetch(`https://pets-v2.dev-apis.com/pets?id=${id}`);
 
 //   obj = await res.json();
 
